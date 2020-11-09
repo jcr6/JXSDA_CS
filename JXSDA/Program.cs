0e16d72f63c007d99cb254b0744cd119464ec2fa
Jeroen Broks <facebook@tbbs.nl>
2020-11-10 00:03:57 +0100

    Test Program

---
+++ JXSDA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyUnits {
+    static class Program {
+
+        const string PackFile = "E:/Projects/Applications/VisualStudio/JXSDA/JXSDA/JXSDA.cs";
+        static readonly byte[] startbuf;
+
+        static Program() {
+            QCol.Doing("Reading", PackFile);
+            startbuf = QuickStream.GetFile(PackFile);
+        }
+
+        static void Main(string[] args) {
+            JXSDA.Verbose = true;
+            var packed = JXSDA.Pack(startbuf);
+            Console.WriteLine($"Packed attempt done. {startbuf.Length} => {packed.Length} => ({(int)Math.Floor(((double)packed.Length/startbuf.Length)*100)}%)");
+            TrickyDebug.AttachWait();
+        }
+    }
+}
