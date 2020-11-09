ae5dee12be6c34982f2cb902dc22d57dda3e0760
Jeroen Broks <facebook@tbbs.nl>
2020-11-10 00:51:57 +0100

    Unpack

--- JXSDA/JXSDA.cs
+++ JXSDA/JXSDA.cs
@@ -1,13 +1,49 @@
-﻿using System;
+// Lic:
+// JXSDA/JXSDA.cs
+// JXDA - Jeroen's eXtremely Simplefied Dictionary Algorithm
+// version: 20.11.10
+// Copyright (C) 2020 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TrickyUnits {
+
+    enum On_JXDA_Error { Ignore, ConsoleMessage,ThrowException}
+
     static class JXSDA {
 
         static public bool Verbose = false;
+        static public On_JXDA_Error OnError = On_JXDA_Error.ConsoleMessage;
+        static void Fout(string M) {
+            switch( OnError){
+                case On_JXDA_Error.ConsoleMessage:
+                    Console.WriteLine($"JXSDA Error: {M}");
+                    return;
+                case On_JXDA_Error.Ignore:
+                    return;
+                case On_JXDA_Error.ThrowException:
+                    throw new Exception($"JXSDA Error {M}");
+            }
+        }
+
         static void P(string m) {
             if (Verbose) {
                 QCol.Cyan("Verbose: ");
@@ -31,7 +67,7 @@ namespace TrickyUnits {
 
         static byte needbit(int s) {
             if (s <= 255) return 8;
-            if (s<= 65535) return 16;
+            if (s <= 65535) return 16;
             //if (s <= 4294967295) return 32;
             //return 64;
             return 32;
@@ -43,7 +79,7 @@ namespace TrickyUnits {
             return tot;
         }
 
-        static double Ratio(int deel,int geheel) {
+        static double Ratio(int deel, int geheel) {
             return (double)deel / (double)geheel;
         }
 
@@ -89,17 +125,17 @@ namespace TrickyUnits {
             byte ubit = 64;
             byte pbit = 64;
             byte uinc = 8;
-            byte pinc = 8;
-            int dsiz = 0;            
+            //byte pinc = 8;
+            int dsiz = 0;
             var rat64 = Ratio(d64.Count, ibuf.Length / 8);
             var rat32 = Ratio(d32.Count, ibuf.Length / 4);
             var rat16 = Ratio(d16.Count, ibuf.Length / 2);
             byte rest = 0;
             P($"Ratio   64:{rat64}; 32:{rat32}; 16:{rat16}");
 
-            if (rat16 < rat64 && rat16 < rat32 && needbit(d16.Count) < 16) { ubit = 16; pbit = needbit(d16.Count); dsiz = (int)d16.Count; rest = mod16; uinc = 2; } else if (rat32 < rat64 && needbit(d32.Count) < 32) { ubit = 32; pbit = needbit(d32.Count); dsiz = (int)d32.Count; rest = mod32; uinc = 4; } 
+            if (rat16 < rat64 && rat16 < rat32 && needbit(d16.Count) < 16) { ubit = 16; pbit = needbit(d16.Count); dsiz = (int)d16.Count; rest = mod16; uinc = 2; } else if (rat32 < rat64 && needbit(d32.Count) < 32) { ubit = 32; pbit = needbit(d32.Count); dsiz = (int)d32.Count; rest = mod32; uinc = 4; }
             P($"Chosen: {ubit} => {pbit}");
-            if (pbit == 64) {
+            if (pbit == 64 || pbit==ubit) {
                 P("Packing not possible, so let's get outta here!");
                 return cpy(ibuf);
             }
@@ -110,6 +146,7 @@ namespace TrickyUnits {
             MS.WriteByte(ubit);
             MS.WriteByte(pbit);
             MS.WriteByte(rest);
+            MS.WriteInt(dsiz);
             for (int i = 0; i < dsiz; i++) {
                 switch (ubit) {
                     case 32:
@@ -129,7 +166,8 @@ namespace TrickyUnits {
                         throw new Exception("Unknown bit setting in creating dictionary (bug?)");
                 }
             }
-            for(int p = 0; p < ibuf.Length-uinc; p += uinc) {
+            for (int p = ibuf.Length - rest; p < ibuf.Length; p++) MS.WriteByte(ibuf[p]);
+            for (int p = 0; p < ibuf.Length - uinc; p += uinc) {
                 long value;
                 switch (ubit) {
                     case 64:
@@ -153,14 +191,93 @@ namespace TrickyUnits {
                     case 16:
                         MS.WriteShort((short)Dict[value]);
                         break;
+                    case 8:
+                        MS.WriteByte((byte)Dict[value]);
+                        break;
                     default:
                         throw new Exception("Unknown bit setting in writing bytes to pack (bug?)");
                 }
             }
-            for (int p = ibuf.Length - rest; p < ibuf.Length; p++) MS.WriteByte(ibuf[p]);
             var ret = PureMemStream.ToArray();
             MS.Close();
             return ret; // temp
+        }
+
+        static public byte[] Unpack(byte[] iBuf) {
+            var PureMemStream = new MemoryStream(iBuf);
+            var MS = new QuickStream(PureMemStream);
+            if (MS.ReadString(6) != "JXSDA\x1a") { Fout("Data is not JXSDA packed data"); return null; }
+            var ubit = MS.ReadByte();
+            var pbit = MS.ReadByte();
+            var rest = MS.ReadByte();
+            var dsiz = MS.ReadInt();
+            var dict = new Dictionary<long, long>();
+            var restbank = new List<byte>();
+            P($"Unpack data: ubit:{ubit}; pbit:{pbit}; rest:{rest}; dsiz:{dsiz}");
+            for(int i = 0; i < dsiz; ++i) {
+                switch (ubit) {
+                    case 64:
+                        dict[i] = MS.ReadLong();
+                        break;
+                    case 32:
+                        dict[i] = MS.ReadInt();
+                        break;
+                    case 16:
+                        dict[i] = MS.ReadShort();
+                        break;
+                    default:
+                        Fout($"Invalid ubit value {ubit} while reading dictionary");
+                        return null;
+                }
+            }
+            for (byte i = 0; i < rest; i++) restbank.Add(MS.ReadByte());
+            var UPWrite = new MemoryStream();
+            var U = new QuickStream(UPWrite);
+            while (!MS.EOF) {
+                try {
+                    long value = 0;
+                    switch (pbit) {
+                        case 32:
+                            value = MS.ReadInt();
+                            break;
+                        case 16:
+                            value = MS.ReadShort();
+                            break;
+                        case 8:
+                            value = MS.ReadByte();
+                            break;
+                        default:
+                            Fout($"Invalud pbit value {pbit} in reading bytes to unpack");
+                            break;
+                    }
+                    if (!dict.ContainsKey(value)) {
+                        Fout($"Undefined dictionary index {value}/{value:X2}");
+                        return null;
+                    }
+                    switch (ubit) {
+                        case 64:
+                            U.WriteLong(dict[value]);
+                            break;
+                        case 32:
+                            U.WriteInt((int)dict[value]);
+                            break;
+                        case 16:
+                            U.WriteShort((short)dict[value]);
+                            break;
+                        default:
+                            Fout($"Invalud ubit value ({ubit}) in writing bytes in unpacking");
+                            return null;
+                    }
+                } catch(Exception e) {
+                    Fout($".NET error unpacking: {e.Message}");
+                    return null;
+                }
+            }
+            foreach (byte b in restbank) U.WriteByte(b);
+            var result = UPWrite.ToArray();
+            MS.Close();
+            U.Close();
+            return result; // temp
         }
     }
 }
--- JXSDA/Program.cs
+++ JXSDA/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+// Lic:
+// ***********************************************************
+// JXSDA/Program.cs
+// This particular file has been released in the public domain
+// and is therefore free of any restriction. You are allowed
+// to credit me as the original author, but this is not
+// required.
+// This file was setup/modified in:
+// 2020
+// If the law of your country does not support the concept
+// of a product being released in the public domain, while
+// the original author is still alive, or if his death was
+// not longer than 70 years ago, you can deem this file
+// "(c) Jeroen Broks - licensed under the CC0 License",
+// with basically comes down to the same lack of
+// restriction the public domain offers. (YAY!)
+// ***********************************************************
+// Version 20.11.10
+// EndLic
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +38,15 @@ namespace TrickyUnits {
         static void Main(string[] args) {
             JXSDA.Verbose = true;
             var packed = JXSDA.Pack(startbuf);
+            var unpacked = JXSDA.Unpack(packed);
             Console.WriteLine($"Packed attempt done. {startbuf.Length} => {packed.Length} => ({(int)Math.Floor(((double)packed.Length/startbuf.Length)*100)}%)");
+            if (unpacked != null) {
+                Console.WriteLine($"Unpacking attempt done. {packed.Length}=>{unpacked.Length} (Size check succes: {unpacked.Length==startbuf.Length})");
+                var s = Encoding.UTF8.GetString(unpacked, 0, unpacked.Length);
+                Console.WriteLine($"<unpacked>\n{s}\n</unpacked>");
+            } else {
+                QCol.QuickError("Something went wrong in unpacking");
+            }
             TrickyDebug.AttachWait();
         }
     }
